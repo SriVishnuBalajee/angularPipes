cd1e97b9826ae6c3c60411a5b629fe37ab3f17f0
srivishnu.balaji <srivishnu.balaji@200411LTP2869>
2023-10-30 09:58:13 +0530

    commit 1

---
+++ src/app/UserData.ts
@@ -0,0 +1,10 @@
+export class UserData{
+    UserData: Array<object> = [];
+    firstname:string="Sri";
+    lastname:string="Vishnu";
+    location:string="Kashmir";
+    company:string="GAVS";
+    salary:number=30000;  
+    DateofJoin:Date=new Date(); 
+ }
+    
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { UserData } from './UserData';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularPipes';
+  uData= new UserData();
 }
